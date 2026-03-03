fa8ecfef2e536bba6f1a93f95b5fa09606ceb165
Mad <163802398+marcosfelpdev@users.noreply.github.com>
2026-03-03 15:25:37 +0000

    chore: page.tsx and layout.tsx files created, tested and implemented

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next';
+import { inter } from '@/app/ui/fonts'
+import React from 'react';
+
+
+export const metadata: Metadata = {
+    title: 'ACME'   
+
+};
+
+export default function RootLayout({ children }:{children: React.ReactNode}) {
+    return (
+        <html lang='pt-br'>
+            <body className={inter.className}>
+                {children}
+            </body>
+        </html>
+    );
+}
