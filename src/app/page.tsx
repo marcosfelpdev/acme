d2ea5f5ee69f69ce68e0defae145f782496b8e01
Mad <163802398+marcosfelpdev@users.noreply.github.com>
2026-03-10 14:49:57 +0000

    chore: configure tailwind styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Page() {
-    return 'Sistema em construção.';
+    return (
+    <main className="bg-red-800">
+        <h1 className="text-blue-800 text-center">Sistema em construção</h1>
+    </main>
+    )
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,13 @@
 import type { Config } from 'tailwindcss';
-
 const config: Config = {
   content: [
-    '.src/app/**/*.{js,jsx,ts,tsx}',
-    '.src/components/**/*.{js,jsx,ts,tsx}'
+    './src/app/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {}
   },
   plugins: []
-
-
 };
 
 export default config;
